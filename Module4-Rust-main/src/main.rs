f929aead86469630e1e3a791a3790e0bd66f1c4c
Michael <gadreelazazel@gmail.com>
2023-07-12 19:21:37 +0100

    Update main.rs

--- Module4-Rust-main/src/main.rs
+++ Module4-Rust-main/src/main.rs
@@ -2,12 +2,14 @@ mod slice;
 mod structs;
 mod matching;
 mod variables;
+mod employee_class;
 
 fn main() {
         //slice::slice_example();
         //structs::struct_examples();
         //matching::matching_examples();
-        variables::variables_example();
+        // variables::variables_example();
+        employee_class::employee_struct_class();
 }
     
     
