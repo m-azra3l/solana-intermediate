59dbdd48439ba95baa1b905ae7b8e33b291d5664
Michael <gadreelazazel@gmail.com>
2023-07-12 22:20:21 +0100

    Update state.rs

--- Rust_Solana_Escrow/program/src/state.rs
+++ Rust_Solana_Escrow/program/src/state.rs
@@ -7,7 +7,13 @@ use solana_program::{
 use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 
 //Create your struct below
-
+pub struct Escrow {
+    pub is_initialized: bool,
+    pub initializer_pubkey: Pubkey,
+    pub temp_token_account_pubkey: Pubkey,
+    pub initializer_token_to_receive_account_pubkey: Pubkey,
+    pub expected_amount: u64,
+}
 
 impl Sealed for Escrow {}
 
