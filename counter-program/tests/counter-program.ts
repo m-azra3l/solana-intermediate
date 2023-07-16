6af7c030db3ed0d84022e6ca5268960088e26ac0
Michael <gadreelazazel@gmail.com>
2023-07-16 22:37:42 +0100

    Update counter-program.ts

--- counter-program/tests/counter-program.ts
+++ counter-program/tests/counter-program.ts
@@ -3,45 +3,55 @@ import { Program } from "@coral-xyz/anchor";
 import { CounterProgram } from "../target/types/counter_program";
 import { Keypair, SystemProgram } from "@solana/web3.js";
 
+// Describe the test suite for the counter-program
 describe("counter-program", () => {
-  anchor.setProvider(anchor.AnchorProvider.env())
-  const provider = anchor.AnchorProvider.env()
-  const program = anchor.workspace.CounterProgram as Program<CounterProgram>
+  // Set the provider to use the environment
+  anchor.setProvider(anchor.AnchorProvider.env());
+  // Get the provider from the environment
+  const provider = anchor.AnchorProvider.env();
+  // Get the CounterProgram from the workspace
+  const program = anchor.workspace.CounterProgram as Program<CounterProgram>;
 
-	// create counter keypair
-  let counter = Keypair.generate()
+  // Create a keypair for the counter account
+  let counter = Keypair.generate();
 
+  // Test case to create a Counter account
   it("Create Counter account!", async () => {
+    // Call the create method of the program
     const tx = await program.methods.create()
-    .accounts({
-      counter: counter.publicKey,
-      authority: provider.wallet.publicKey,
-      systemProgram: SystemProgram.programId
-    })
-    .signers([counter])
-    .rpc()
-    console.log("Your transaction signature", tx)
+      .accounts({
+        counter: counter.publicKey,
+        authority: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      })
+      .signers([counter])
+      .rpc();
+    console.log("Your transaction signature", tx);
   });
 
+  // Test case to increment the Counter
   it("Increment Counter!", async () => {
+    // Call the increment method of the program
     const tx = await program.methods.increment()
-    .accounts({
-      counter: counter.publicKey,
-      authority: provider.wallet.publicKey,
-    })
-    .signers([counter])
-    .rpc()
+      .accounts({
+        counter: counter.publicKey,
+        authority: provider.wallet.publicKey,
+      })
+      .signers([counter])
+      .rpc();
     console.log("Your transaction signature:", tx);
   });
 
+  // Test case to decrement the Counter
   it("Decrement Counter!", async () => {
+    // Call the decrement method of the program
     const tx = await program.methods.decrement()
-    .accounts({
-      counter: counter.publicKey,
-      authority: provider.wallet.publicKey,
-    })
-    .signers([counter])
-    .rpc()
+      .accounts({
+        counter: counter.publicKey,
+        authority: provider.wallet.publicKey,
+      })
+      .signers([counter])
+      .rpc();
     console.log("Your transaction signature:", tx);
   });
 });
