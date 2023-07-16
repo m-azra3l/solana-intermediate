8933d1eeb10cec03a13d49a6988f612e3821529e
Michael <gadreelazazel@gmail.com>
2023-07-16 21:26:33 +0100

    Update lib.rs

--- counter-program/programs/counter-program/src/lib.rs
+++ counter-program/programs/counter-program/src/lib.rs
@@ -9,7 +9,36 @@ pub mod counter_program {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn create(ctx: Context<Create>) -> Result<()> {
+        ctx.accounts.counter.authority = ctx.accounts.authority.key();
+        ctx.accounts.counter.count = 0;
+
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        // TODO
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = authority, space = <space needed in bytes>)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct Counter {
+    pub authority: Pubkey,
+    pub count: u64,
+}
+
+
