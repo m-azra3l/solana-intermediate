5cca168aba4823423463900adf99453ba323dbed
Michael <gadreelazazel@gmail.com>
2023-07-24 15:21:08 +0100

    Update lib.rs

--- hello-world/programs/hello-world/src/lib.rs
+++ hello-world/programs/hello-world/src/lib.rs
@@ -6,7 +6,7 @@ declare_id!("81BZshqgucT57iAw4kgPZiGpApZS4LbBzTXT2UMEAzod");
 pub mod hello_world {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         msg!("Hello Metacrafter!");
         Ok(())
     }
