400eebee2c8cad4f42db8f2cf9efc80bc94b50b6
Michael <gadreelazazel@gmail.com>
2023-07-24 16:52:36 +0100

    Update virtual-wallet.ts

--- virtual-wallet/tests/virtual-wallet.ts
+++ virtual-wallet/tests/virtual-wallet.ts
@@ -1,16 +1,47 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { VirtualWallet } from "../target/types/virtual_wallet";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import assert from "assert";
 
 describe("virtual-wallet", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.VirtualWallet as Program<VirtualWallet>;
+  const provider = anchor.AnchorProvider.env();
 
-  it("Is initialized!", async () => {
+  it("Initialize Virtual Wallet!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+
+    // derive cash pda
+    const [cash, cashBump]  = await PublicKey.findProgramAddress(
+      [provider.wallet.publicKey.toBuffer(), Buffer.from("cash")], 
+      program.programId,
+    );
+
+    // derive credit card pda
+    const [creditCard, ccBump]  = await PublicKey.findProgramAddress(
+      [provider.wallet.publicKey.toBuffer(), Buffer.from("credit-card")], 
+      program.programId,
+    );
+
+    // derive dedit card pda
+    const [deditCard, dbBump]  = await PublicKey.findProgramAddress(
+      [provider.wallet.publicKey.toBuffer(), Buffer.from("dedit-card")], 
+      program.programId,
+    );
+
+    const tx = await program.methods.initializeWallet()
+    .accounts({
+      user: provider.wallet.publicKey,
+      cash: cash,
+      creditCard: creditCard,
+      deditCard: deditCard,
+      systemProgram: SystemProgram.programId
+    })
+    .rpc();
+    console.log("Virtual Wallet Initialized!");
     console.log("Your transaction signature", tx);
   });
 });
