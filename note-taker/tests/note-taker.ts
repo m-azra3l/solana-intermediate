c54425bef4c02718d5dfa1871e555ea8baa6997e
Michael <gadreelazazel@gmail.com>
2023-07-25 09:25:21 +0100

    Update note-taker.ts

--- note-taker/tests/note-taker.ts
+++ note-taker/tests/note-taker.ts
@@ -1,16 +1,30 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
 import { NoteTaker } from "../target/types/note_taker";
+import { Keypair, PublicKey, SystemProgram } from '@solana/web3.js'
 
 describe("note-taker", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.NoteTaker as Program<NoteTaker>;
+  const provider = anchor.AnchorProvider.env()
+  const notesProgram = new PublicKey("<program_id-of-your-notes-program>")
 
-  it("Is initialized!", async () => {
+  const message = "Hello Metacrafter!"
+  const note = Keypair.generate()
+
+  it("Creating note!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.takeNote(message)
+    .accounts({
+      note: note.publicKey,
+      authority: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+      notesProgram: notesProgram
+    })
+    .signers([note])
+    .rpc();
     console.log("Your transaction signature", tx);
   });
 });
